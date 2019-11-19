04da1d5a534a007db67001ceaf1b06aa94e15ef5
Karen Poon <poonyanlam@gmail.com>
2019-11-19 10:45:03 +0000

    folder name change

---
+++ tests/square_test/square_test.ino
@@ -0,0 +1,54 @@
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+Adafruit_DCMotor *motor_left = AFMS.getMotor(1);
+Adafruit_DCMotor *motor_right = AFMS.getMotor(2);
+
+void setup() {
+    AFMS.begin();
+}
+
+void move_forward() {
+    motor_left->setSpeed(50);
+    motor_right->setSpeed(50);
+    motor_left->run(FORWARD);
+    motor_right->run(BACKWARD);
+    delay(3000);
+}
+
+void move_backward() {
+    motor_left->setSpeed(50);
+    motor_right->setSpeed(50);
+    motor_left->run(BACKWARD);
+    motor_right->run(FORWARD);
+    delay(3000);
+}
+
+void clockwise_90() {
+    motor_left->setSpeed(50);
+    motor_right->setSpeed(0);
+    motor_left->run(FORWARD);
+    delay(13000);
+}
+
+void anticlockwise_90() {
+    motor_left->setSpeed(50);
+    motor_right->setSpeed(0);
+    motor_left->run(BACKWARD);
+    delay(13000);
+}
+
+void stop_motors() {
+    motor_left->setSpeed(0);
+    motor_right->setSpeed(0);
+    delay(100000);
+}
+
+void loop() {
+    move_forward();
+    clockwise_90();
+//     stop_motors();
+
+}
