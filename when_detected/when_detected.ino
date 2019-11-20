727b93df8a8bb0818f71a48eaa85b7e3c8adda27
Non Geraint <nlg29@cam.ac.uk>
2019-11-20 19:30:58 +0000

    created file to write test code for after mine detected

---
+++ when_detected/when_detected.ino
@@ -0,0 +1,12 @@
+//assuming distance is defined
+//set a new distance variable?
+//move 15 cm to check for any obstructions
+//if yes, turn there (break!)
+//else reverse 15 cm 
+//turn 90 degrees
+//travel distance-5 (check where this puts us, could be useful)
+//add homing idea here
+//hall effect code goes here
+//pick up code goes here
+//reverse distance-5
+//turn 90 degrees
