d8bfa34bf5308f5b8adaa7fb2b110ef0f95e71f0
Non Geraint <nlg29@cam.ac.uk>
2019-11-20 19:50:17 +0000

    finished most of mine detected function

--- when_detected/when_detected.ino
+++ when_detected/when_detected.ino
@@ -1,12 +1,28 @@
-//assuming distance is defined
-//set a new distance variable?
-//move 15 cm to check for any obstructions
-//if yes, turn there (break!)
-//else reverse 15 cm 
-//turn 90 degrees
-//travel distance-5 (check where this puts us, could be useful)
-//add homing idea here
-//hall effect code goes here
-//pick up code goes here
-//reverse distance-5
-//turn 90 degrees
+void mine_detected {
+  int mine_distance;
+  //bool closer_mine = false;
+  //the function below needs to be changed to however sensors and motor simultaneously worked
+  //I can hard code this if needed, e.g.:
+  mine_distance = distance;
+  //start_motors(speed);
+  for (int x = 1; x < 300; x = x + 1){
+    if (distance < mine_distance) {
+      //closer_mine = true;
+      break;
+    }
+    if (x == 150) {
+      //reverse motors
+    }
+    delay(10);
+  }
+  anticlockwise_90();
+  //homing will go here
+  //another for loop? for distance-5 divided by speed time increments
+  //travel, checking distance2 is less than distance-5
+  //if not, turning
+  move_forward(100, distance-5);
+  //hall effect code goes here
+  //pickup code goes here
+  move_backward(100, distance-5);
+  clockwise_90();
+}
