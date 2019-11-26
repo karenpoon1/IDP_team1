a2177befe410963fe868c27154b0bf1b5578a9ff
Karen Poon <poonyanlam@gmail.com>
2019-11-26 11:01:44 +0000

    add sets of movement

--- main/movement_set.h
+++ main/movement_set.h
@@ -4,3 +4,31 @@ void initial_movement() {
     anticlockwise_90();
     move_distance_backward(20);
     }
+
+void after_grip_movement() {
+    move_backward();
+    while (true) {
+        detect_mine();
+        actual_detect(10);
+            if (back_wall_detected) {
+                break;
+            }
+    move_distance_backward();
+    move_distance_forward();
+    clockwise_90();
+    while (true) {
+        detect_mine();
+        actual_detect(10);
+            if (back_wall_detected) {
+                break;
+            }
+    }
+    move_distance_backward();
+    move_distance_forward();
+    clockwise_90();
+    // drop mine
+}
+void after_drop() {
+    anticlockwise_90();
+    move_distance_backward();
+}
