3c11c2194ff77ba50b4cb0d10fc6e87491b9458d
Karen Poon <poonyanlam@gmail.com>
2019-11-27 10:38:11 +0000

    spacing

--- main/movement_set.h
+++ main/movement_set.h
@@ -1,5 +1,6 @@
 
 void initial_movement() {
+    move_distance_backward(10);
     move_distance_forward(10);
     anticlockwise_90();
     move_distance_backward(20);
--- main/servo.h
+++ main/servo.h
@@ -2,23 +2,22 @@
 #include <Servo.h> 
 
 void servo_setup() {
-   int servoPin = 9;
-   Servo servo;  
-   int angle = 0;//current servo angle
-   servo.attach(servoPin);
+    int servoPin = 9;
+    Servo servo;
+    int angle = 0;//current servo angle
+    servo.attach(servoPin);
 }
 
 void servo_forward (int inputangle) {
-  for(angle = 0; angle < inputangle; angle++)  {                                  
-        servo.write(angle);               
+    for(angle = 0; angle < inputangle; angle++) {
+        servo.write(angle);
         delay(15);                   
-      } 
+    }
 }
 
 void servo_backward (int inputangle) {
-  for(angle = inputangle; angle > 0; angle--) {
+    for(angle = inputangle; angle > 0; angle--) {
         servo.write(angle);           
         delay(15);                               
-  }
-            
+    }
 }
