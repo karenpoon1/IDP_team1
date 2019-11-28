b11f3d6b47ca048a3ab4bc3d44f72860b4570b67
Karen Poon <poonyanlam@gmail.com>
2019-11-28 10:55:04 +0000

    commented main.ino

--- main/main.ino
+++ main/main.ino
@@ -25,7 +25,10 @@ void setup() {
 
 void loop() {
     if (mine_counter == 0) {
+        // now robot is facing direction towards the known mine so it moves forward
         move_forward();
+        // while loop to detect mine in front, and will stop when mine is ___ cm in front (the distance in front is set by
+        // the argument in the "actual detect()" function)
         while (true) {
             detect_mine();
             actual_detect(5);
@@ -34,55 +37,24 @@ void loop() {
             }
         }
         stop_motors();
+    }
+        // the following is supposed to be a mine pole detection function and mine gripping function, which have to be written
+        // detect_mine_orientation();
+        // if oriented correctly
+        // flip_mine();
+        // grip_mine();
+        // after_grip_movement(); (in movement_set.h)
 
-        // detect mine orientation 
-        // flip / grip mine
-
-//        move_forward();
-        delay(1000000);
+    // following navigation sequences are to be added
     if (mine_counter > 0 && mine_counter < 7) {
         while (true) {
             }
     }
     mine_counter++;
-
-    }
 }
 
-}
 
-void loop() {
-  if (mine_counter == 0) {
-    //move_forward();
-    while (true) {
-      detect_mine();
-      actual_detect(5);
-      if (mine_detected){
-        break;
-      }
-    }
-    //detect mine orientation 
-    //flip / grip mine
-    //move_forward();
-    delay(1000000);
-    if (mine_counter > 0 && mine_counter < 7) {
-      while (true) {
-        //something here?
-      }
-    }
-    mine_counter++;
-  }
-  move_forward();
-  while (true) {
-    detect_mine();
-    //while (old_mine_distance > 80 && new_mine_distance > 80) {
-    //detect_mine();
-  }
-  //stop_motors();
-}
-//    stop_motors();
-//    delay(1000000);
-//}
+// the following, im thinking how to check mine forward, but am thinking of a better algorithm which i will discuss with you later
 //}
 //     check_mine_forward();
 //     mine_counter++
