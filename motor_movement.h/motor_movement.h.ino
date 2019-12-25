ccf7d125f1ad48636c7c32a3a9d999448dde5082
Karen Poon <poonyanlam@gmail.com>
2019-12-25 14:53:08 +0800

    delete duplicated files

--- motor_movement.h/motor_movement.h.ino
+++
@@ -1,7 +0,0 @@
-#include <Wire.h>
-#include <Adafruit_MotorShield.h>
-#include "utility/Adafruit_MS_PWMServoDriver.h"
-
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor *motor_left = AFMS.getMotor(1);
-Adafruit_DCMotor *motor_right = AFMS.getMotor(2);
